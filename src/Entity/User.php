566d1c8c9556f88984326c11b1b3fdd9a639bf6b
JeremyGhes <j.ghesquiere@allianceautomotive.fr>
2024-05-22 16:50:49 +0200

    Correction SymfonyInsight

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -176,8 +176,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function eraseCredentials(): void
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        
     }
 
     public function getCustomer(): Customer
